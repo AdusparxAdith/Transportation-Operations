80f91458ec07443ac67f9b7cce36287a98dca279
Adith Dinesh <adithdinesh97@gmail.com>
2018-04-23 22:45:09 +0530

    Edited Calamity class file

--- Calamity.py
+++ Calamity.py
@@ -1,12 +1,28 @@
 class Calamity:
-    
+
     def __init__(self, danger_level, duration):
         self.danger_level=danger_level
         self.duration=duration
 
     def Calamity_Type(self):
-        if(self.danger_level>5):
+        if(self.danger_level>5 and self.danger_level<10 ):
             self.calamity_type="Rain"
+        elif(self.danger_level>10 and self.danger_level<15 ):
+            self.calamity_type="Lightning"
+        elif(self.danger_level>15 and self.danger_level<20 ):
+            self.calamity_type="High Tides"
+        elif(self.danger_level>20 and self.danger_level<25 ):
+            self.calamity_type="Cyclone"
+        elif(self.danger_level>25 and self.danger_level<30 ):
+            self.calamity_type="Hurricane"
+        else:
+            self.calamity_type="Unidentified"
+
+    def Danger_Factor(self):
+
+
+
+
 
 
 C=Calamity(6,10)
