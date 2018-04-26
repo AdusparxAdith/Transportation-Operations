92dc928bda49f2c2ba21fd4bae3c794ef7aa4931
SankethBennur <snkthbnnr3@gmail.com>
2018-04-26 22:51:54 +0530

    Add files via upload

--- OperationsMain.py
+++ OperationsMain.py
@@ -1,13 +1,22 @@
-distance = input("What is the distance between the source and your destination? ")
+ships= {
+    "cargo": 20,
+    "crude": 25,
+    "tanker": 30,
+    "ultramax": 35,
+    "ferry": 40
+}
 
-ships= {"cargo": 20,
-        "crude": 25,
-        "tanker": 30,
-        "ultramax": 35,
-        "ferry": 40
-        }
+distance = float(input("What is the distance between the source and your destination? "))
 
-for index in ships:
-    print("\n",index,ships[index])
+# Call misc.py
 
-ship = input("\n\nWhich ship would you like to use? ")
+# for index in ships:
+#         print("\n",index,ships[index])
+
+ship = input("\nWhich ship would you like to use? ")
+
+ship_speed=ships.get(ship)
+
+for i in len(distance_section):
+    PDanger=rand.uniform(0,100)
+    C=Calamity(ship_speed,PDanger)
--- formula_test.py
+++ formula_test.py
@@ -2,62 +2,47 @@ import random
 
 class Calamity:
 
-# Create constructor for CALAMITY object, taking parameters:
-# ship_speed,PDanger
+    # def __init__(self,ship_speed,PDanger):
+        if(PDanger>=79 and PDanger<100):
+            print("It's a Clear Weather. Full Sails!")
+            danger_level=random.uniform(0,20)
+            duration=random.uniform(0,3.5)
+        elif(PDanger>=58 and PDanger<79):
+            print("It's raining! Sailing Smoothly.")
+            danger_level=random.uniform(20,40)
+            duration=random.uniform(1,3.5)
+        elif(PDanger>=38 and PDanger<58):
+            print("Encountered Storm!! It's thundering. Gotta get back into my cabin!")
+            danger_level=random.uniform(40,60)
+            duration=random.uniform(2,3.5)
+        elif(PDanger>=20 and PDanger<38):
+            print("Brace Yourselves for High Tides!!")
+            danger_level=random.uniform(60,80)
+            duration=random.uniform(3.5,5)
+        elif(PDanger>=8 and PDanger<20):
+            print("Moving towards a Cyclone. Take down the sails!!")
+            danger_level=random.uniform(80,100)
+            duration=random.uniform(4,5)
+        else:
+            print("Mother of God, it's a Hurricane!!")
+            danger_level=random.uniform(100,120)
+            duration=random.uniform(4,5)
 
-#This is bcoz we're gonna create an ARRAY OF OBJECTS
+    # def Danger_Speed(self, danger_level, duration):
+        if(PDanger<79): #if not a clear Weather
+            Danger= (danger_level * duration)/10
+            speed= ship_speed - Danger
+        else:
+            profit= (danger_level * duration)/10
+            speed= ship_speed + profit
+        if(speed>0):
+            print("Speed ",speed)
+        else:
+            halt=True
+            print("Halt")
 
-# ship_speed, from main function
+    # if(!halt):
+    #     time=distance_section[j]/speed
 
-# PDanger=random.uniform(0,99), again from main function
-
-# Inside the constructor, the if-else clause begins:
-    if(PDanger>=79 and PDanger<100):
-        print("It's a Clear Weather. Full Sails!")
-        danger_level=random.uniform(0,20)
-        duration=random.uniform(0,3.5)
-    elif(PDanger>=58 and PDanger<79):
-        print("It's raining! Sailing Smoothly.")
-        danger_level=random.uniform(20,40)
-        duration=random.uniform(1,3.5)
-    elif(PDanger>=38 and PDanger<58):
-        print("Encountered Storm!! It's thundering. Gotta get back into my cabin!")
-        danger_level=random.uniform(40,60)
-        duration=random.uniform(2,3.5)
-    elif(PDanger>=20 and PDanger<38):
-        print("Brace Yourselves for High Tides!!")
-        danger_level=random.uniform(60,80)
-        duration=random.uniform(3.5,5)
-    elif(PDanger>=8 and PDanger<20):
-        print("Moving towards a Cyclone. Take down the sails!!")
-        danger_level=random.uniform(80,100)
-        duration=random.uniform(4,5)
-    else:
-        print("Mother of God! It's a Hurricane!!")
-        danger_level=random.uniform(100,120)
-        duration=random.uniform(4,5)
-
-    # funciton for calculating Danger and speed
-    #
-    if(PDanger<79): #if not a clear Weather
-        Danger= (danger_level * duration)/10
-        speed= ship_speed - Danger
-    else: #if it is a clear weather, ship should accelerate
-        profit= (danger_level * duration)/10
-        speed= ship_speed + profit
-
-    #This is optional
-    #We only want to test if the formulae and the values are proper
-    #We shall create 2 functions to return "Danger" and "speed"
-    #Those 2 results will be returned in the main function
-    
-    print("Danger Level: ", danger_level)
-    print("Duration: ",duration)
-
-    if(PDanger<79): #Clear weather not considered a Danger
-        print("Danger: ",Danger)
-        
-    if(speed>0):
-        print("Speed ",speed)
-    else:
-        print("Halt")
+    # else:
+    #     PDanger=rand.uniform(0,100)
