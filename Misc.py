d013baa25fa37d05fa98e3c12ce32dcac730f931
SankethBennur <snkthbnnr3@gmail.com>
2018-05-03 23:53:22 -0700

    Add files via upload

--- Misc.py
+++ Misc.py
@@ -1,20 +1,66 @@
 import random
+import formula_test3
+
+simulated_interval=[]
 
 def Condition_Generator(distance):
-    if(distance > 2000):
-        distance_section = int(distance / 500)
+    dist_interval=[]
+
+    if(distance>2000):
+        while(True):
+            dist_interval.append(500.0)
+            distance=distance-500
+            if(distance<500):
+                dist_interval.append(distance)
+                break
     else:
-        distance_section = distance/4
+        dist_interval=[distance/4] *4
+    return dist_interval
 
-def Simulation(ship_speed,distance):
-    for i in len(distance_section):
-        PDanger=rand.uniform(0,100)
-        C=Calamity(ship_speed,PDanger)
+def Simulation(weight,ship_speed,distance):
+    # Give time Interval
+    simulated_interval=Condition_Generator(distance)
+    print("Interval: ",simulated_interval)
+    #
+    # for i in simulated_interval:
+    #     simulated_interval[i]*= 1.852
+    #
+    # print("Interval in km: ",simulated_interval)
+    Resultant_Time=0
 
-distance_section=[]     # 500  500  500  500 500 500 500 430
-len(distance_section)
+    for i in range(0,len(simulated_interval)):
+        PDanger=random.uniform(0,100)
+        C=formula_test3.Calamity(weight,ship_speed,PDanger)
 
-    occurances = random.randrange(0,distance_section+1)
-    return(occurances)
+        speed=C.Speed()
 
-Simulation(400)
+
+        if(speed>0):
+            speed = round(speed, 2)
+            print("Sailing at the speed of ",speed," knots")
+            time=simulated_interval[i]/speed
+            print("Time: ",time)
+            Resultant_Time += time
+        else:
+            time=(random.uniform(3.5,5))
+            print("Halting for ",time)
+            Resultant_Time += time
+            i=i-1
+            continue
+
+    def Time(Resultant_Time):
+          Days = Resultant_Time/ 24
+          hours = Days - int(Days)
+          hours *= 24
+
+          minutes = hours - int(hours)
+          minutes *= 60
+
+          seconds = minutes - int(minutes)
+          seconds *= 60
+
+          print(int(Days),"day(s)",int(hours),"hours",int(minutes),"minutes",int(seconds),"seconds")
+
+    Time(Resultant_Time)
+
+    # print("Hours: ",Resultant_Time)
--- OperationsMain.py
+++ OperationsMain.py
@@ -1,23 +1,47 @@
-ships= {
-    "cargo": 20,
-    "crude": 25,
-    "tanker": 30,
-    "ultramax": 35,
-    "ferry": 40
+import Misc
+
+ships = { "Tanker" : 15.0,	#DWT 200
+	"Cargo" : 20.0,		#DWT 120
+	"Passenger Ship" : 30.0,	#DWT 80
+	"Frigate" : 35.0,	#DWT 60
+	"Ferry" : 40.0,		#DWT 50
+	"High Speed Craft" : 50.0	#DWT 40
 }
+
+# 1 Nautical Mile (nm) = 1.852 Km
+# 1 Knot= 1.852 km/hr
 
 distance = float(input("What is the distance between the source and your destination? "))
 
-# Call misc.py
-
-# for index in ships:
-#         print("\n",index,ships[index])
-
+print (ships)
 ship = input("\nWhich ship would you like to use? ")
-s
 ship_speed=ships.get(ship)
-#
-#Call Simulation
-# for i in len(distance_section):
-#     PDanger=rand.uniform(0,100)
-#     C=Calamity(ship_speed,PDanger)
+
+# for i in ships:
+if(ship not in ships):
+    print("Invalid option.")
+    exit(0)
+
+# Selecting weights of  ships
+print("Select the weight of the ship: ")
+
+def weights():
+    # Have to make the error clauses too
+    if(ship=="Tanker"):
+        print("~120-200")
+    if(ship=="Cargo"):
+        print("~80-120")
+    if(ship=="Passenger Ship"):
+        print("~60-80")
+    if(ship=="Frigate"):
+        print("~50-60")
+    if(ship=="Ferry"):
+        print("~45-50")
+    if(ship=="High Speed Craft"):
+        print("~40-45")
+
+    weight=float(input())
+    return weight
+
+
+Misc.Simulation(weights(),ship_speed,distance)
